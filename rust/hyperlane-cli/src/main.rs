e9fb882fc61c4f90d363c3961815e80f1e48bafc
Nicholas Chee <nicholas.yang.chee@gmail.com>
2024-06-03 18:30:09 -0400

    feat: Add CLI function to listen for outgoing messages
    Added a function to the CLI which listens for new dispatch events at the target Mailbox.
    Events are filtered according to a MatchList provided by the user.
    Running `hyperlane-cli search` describes usage.

--- rust/hyperlane-cli/src/main.rs
+++ rust/hyperlane-cli/src/main.rs
@@ -1,60 +1,102 @@
 use std::sync::Arc;
 
 use ethers::{
-    prelude::SignerMiddleware,
-    providers::{Http, Middleware, Provider},
+    core::types::{Address, Bytes, H256},
+    prelude::{LocalWallet, SignerMiddleware},
+    providers::{Http, Middleware, Provider, StreamExt},
     signers::Signer,
 };
 
+use hyperlane_core::HyperlaneMessage;
+
 mod args;
 mod mailbox;
+mod matching_list;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = args::Args::new()?;
-
+async fn dispatch(
+    rpc_url: String,
+    mailbox_addr: Address,
+    pkey: LocalWallet,
+    dest_chain_id: u32,
+    recip_addr: H256,
+    msg: Bytes,
+) -> Result<(), Box<dyn std::error::Error>> {
     // Connect to the origin chain and determine it's chain ID
-    let provider = Arc::new(Provider::<Http>::try_from(args.rpc_url.clone())?);
+    let provider = Arc::new(Provider::<Http>::try_from(rpc_url.clone())?);
     let origin_chain_id = provider.get_chainid().await?.as_u32();
     println!(
         "Connected to network via {}, chain ID is {}",
-        args.rpc_url, origin_chain_id
+        rpc_url, origin_chain_id
     );
 
     // Build an interface to the origin chain's mailbox,
     // include signer middleware in order to send transactions
-    let wallet = args.pkey.with_chain_id(origin_chain_id);
+    let wallet = pkey.with_chain_id(origin_chain_id);
     let provider = Arc::new(SignerMiddleware::new(provider, wallet));
-    let contract = mailbox::Mailbox::new(args.mailbox_addr, provider.clone());
+    let contract = mailbox::Mailbox::new(mailbox_addr, provider.clone());
 
-    /*
-     * TODO: Determine the proper formula for gas calculation.
-     * Currently, the gas used in a transaction is the sum of
-     * ether-rs' gas estimation and the mailbox's quote.
-     */
     // Receive a quote from mailbox for the dispatched message's gas cost
-    let quote_dispatch = contract.quote_dispatch(
-        args.dest_chain_id,
-        args.recip_addr.to_fixed_bytes(),
-        args.msg.clone(),
-    );
+    let quote_dispatch =
+        contract.quote_dispatch(dest_chain_id, recip_addr.to_fixed_bytes(), msg.clone());
     let quote = quote_dispatch.call().await?;
     println!("Mailbox's gas quote for this message is: {quote}");
 
     // Assemble dispatch transaction
-    let dispatch = contract.dispatch_0(
-        args.dest_chain_id,
-        args.recip_addr.to_fixed_bytes(),
-        args.msg.clone(),
-    );
+    let dispatch = contract.dispatch_0(dest_chain_id, recip_addr.to_fixed_bytes(), msg.clone());
     let gas_price = provider.get_gas_price().await?;
-    let gas = dispatch.estimate_gas().await? + quote;
+    println!("Average gas price: {gas_price}");
+    let gas = dispatch.estimate_gas().await?;
     println!("Transaction gas estimate: {gas}\nAverage gas price: {gas_price}");
     let dispatch = dispatch.gas(gas).gas_price(gas_price);
 
     // Send dispatch transaction
     let tx_hash = dispatch.send().await?.tx_hash();
     println!("Dispatch transaction is pending: https://etherscan.io/tx/{tx_hash:?}");
-
     Ok(())
+}
+
+async fn search(
+    rpc_url: String,
+    mailbox_addr: Address,
+    matching_list: matching_list::MatchingList,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let provider = Arc::new(Provider::<Http>::try_from(rpc_url.clone())?);
+    let origin_chain_id = provider.get_chainid().await?.as_u32();
+    println!(
+        "Connected to network via {}, chain ID is {}",
+        rpc_url, origin_chain_id
+    );
+
+    // Build an interface to the origin chain's mailbox,
+    let contract = mailbox::Mailbox::new(mailbox_addr, provider.clone());
+
+    let event = contract.event::<mailbox::DispatchFilter>();
+    let mut event_stream = event.stream().await?;
+
+    while let Some(Ok(dispatch_filter)) = event_stream.next().await {
+        let hyperlane_msg: HyperlaneMessage = dispatch_filter.message.to_vec().into();
+        if matching_list.msg_matches(&hyperlane_msg, false) {
+            println!("{:?}", hyperlane_msg);
+        }
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match args::Args::new()? {
+        args::Args::Dispatch {
+            rpc_url,
+            mailbox_addr,
+            pkey,
+            dest_chain_id,
+            recip_addr,
+            msg,
+        } => dispatch(rpc_url, mailbox_addr, pkey, dest_chain_id, recip_addr, msg).await,
+        args::Args::Search {
+            rpc_url,
+            mailbox_addr,
+            matching_list,
+        } => search(rpc_url, mailbox_addr, matching_list).await,
+    }
 }
